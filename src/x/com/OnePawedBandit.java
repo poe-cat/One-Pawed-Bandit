e5dbd4d56df80c014fe5d1ec1f00ff124b535406
Kasia <katarzyna.tramea@gmail.com>
2022-02-22 13:40:39 +0100

    delete unnecessary parts

--- src/x/com/OnePawedBandit.java
+++ src/x/com/OnePawedBandit.java
@@ -16,23 +16,20 @@ import javafx.stage.Stage;
 
 public class OnePawedBandit extends Application {
 
-    GridPane gridPane;
-
-    //layouts
-    VBox col1, col2, col3;
-    HBox row1, row2;
-
-    TextField yourAmount;
-    int totalValue = 0;
-    ImageView imageView1, imageView2, imageView3, defaultImg;
-    Label wonSpin, wonTotal;
+    private GridPane gridPane;
+    private VBox col1, col2, col3;
+    private HBox row1, row2;
+    private TextField yourAmount;
+    private int totalValue = 0;
+    private ImageView imageView1, imageView2, imageView3, defaultImg;
+    private Label wonSpin, wonTotal;
 
     @Override
     public void start(Stage primaryStage) {
 
         primaryStage.setTitle("ONE-PAWED BANDIT");
 
-        gridPane = new GridPane(); //as root pane
+        gridPane = new GridPane();
         gridPane.setPadding(new Insets(10, 50, 50, 50));
         gridPane.setHgap(10);
         gridPane.setVgap(12);
@@ -42,7 +39,6 @@ public class OnePawedBandit extends Application {
         gridPane.add(row1, 0, 0);
         gridPane.add(row2, 0, 1);
 
-        //default image until game starts
         String path = "x/com/avocato.jpg";
         Image image = new Image(path);
         defaultImg = new ImageView(image);
@@ -58,34 +54,28 @@ public class OnePawedBandit extends Application {
         col2.setPadding(new Insets(10, 10, 10, 10));
         col3.setPadding(new Insets(10, 10, 10, 10));
 
-        row2.getChildren().addAll(col1, col2, col3); //add all columns to 2nd row
+        row2.getChildren().addAll(col1, col2, col3);
 
-        //button to "spin"
         Button spinButton = new Button();
         spinButton.setStyle("-fx-background-color: #00cc00;");
         spinButton.setText("SPIN");
 
-        //all about money
         Label putAmount = new Label("Declare amount: ");
         yourAmount = new TextField();
 
         wonSpin = new Label("Won on this round: zł");
         wonTotal = new Label("Your total prize: zł");
 
-        //if text field is empty
         Label emptyField = new Label("You must declare amount first!");
         emptyField.setVisible(false);
 
-        //add labels and button to columns
         col1.getChildren().addAll(putAmount, yourAmount, emptyField);
         col2.getChildren().addAll(spinButton);
         col3.getChildren().addAll(wonSpin, wonTotal);
 
-        //add gridPane to scene
         primaryStage.setScene(new Scene(gridPane, 700, 350));
         primaryStage.show();
 
-        //event handler for Button
         spinButton.setOnAction(event -> {
 
             emptyField.setVisible(false);
@@ -95,7 +85,6 @@ public class OnePawedBandit extends Application {
 
                 displayImgAndPoints(getRandomInt(10, 1), getRandomInt(10, 1),
                         getRandomInt(10, 1), spinAmt);
-                //txtAmt.setText("");
             } catch (NumberFormatException e) {
                 emptyField.setVisible(true);
             }
@@ -104,7 +93,7 @@ public class OnePawedBandit extends Application {
 
     public void displayImgAndPoints(int random1, int random2, int random3, int spinAmt) {
 
-        row1.getChildren().clear(); //clear previous image(s)
+        row1.getChildren().clear();
 
         //image 1
         String path = "/images/" + random1 + ".jpg";
@@ -124,7 +113,6 @@ public class OnePawedBandit extends Application {
         imageView3 = new ImageView(image);
         row1.getChildren().addAll(imageView3);
 
-        //images size
         imageView1.setFitHeight(200);
         imageView1.setFitWidth(200);
         imageView2.setFitHeight(200);
@@ -133,29 +121,23 @@ public class OnePawedBandit extends Application {
         imageView3.setFitWidth(200);
 
 
-        //points per spin
         if(random1 == random2 && random1 == random3) {
             spinAmt = spinAmt * 3;
         } else
         if(random1 == random2 || random1 == random3 || random2 == random3) {
             spinAmt = spinAmt * 2;
         } else
-            spinAmt = 0; //no identical images in spin
+            spinAmt = 0;
 
 
-        totalValue = totalValue + spinAmt;  //total cash amount for all spins
+        totalValue = totalValue + spinAmt;
 
-        wonSpin.setText("Won on this turn: " + spinAmt + " zł"); //display result (cash) of recent spin
-        wonTotal.setText("Your total prize: " + totalValue + " zł"); //display total result
+        wonSpin.setText("Won on this turn: " + spinAmt + " zł");
+        wonTotal.setText("Your total prize: " + totalValue + " zł");
 
     }
 
     public int getRandomInt(int max, int min) {
-        return ((int)(Math.random() * (max - min))) + min;  //returns random number to randomize images
-    }
-
-
-    public static void main(String[] args) {
-        launch();
+        return ((int)(Math.random() * (max - min))) + min;
     }
 }
